29c535531524b9773cd49d62906b24b585280b78
mastercodergautam <125134265+mastercodergautam@users.noreply.github.com>
2023-07-07 15:08:11 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRUpoUxBhGiHg9_hMjfvkNyLp0sRJnto8",
+    authDomain: "lets-chat-39381.firebaseapp.com",
+    databaseURL: "https://lets-chat-39381-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-39381",
+    storageBucket: "lets-chat-39381.appspot.com",
+    messagingSenderId: "764648023204",
+    appId: "1:764648023204:web:1026800faabc31b4b87231"
+  };
+  
+  // Initialize Firebase
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
